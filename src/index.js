2f52287202c53223890c929e4dfa916dcfc2a1d8
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-11 22:26:36 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,72 +1,65 @@
 let useDjsMaster = false;
 try {
-    const DjsVersion = require('discord.js').version;
-    if(DjsVersion.split('.').shift() === '12') useDjsMaster = true;
-} catch(e) {
-    useDjsMaster = false;
+  const DjsVersion = require("discord.js").version;
+  if (DjsVersion.split(".").shift() === "12") useDjsMaster = true;
+} catch (e) {
+  useDjsMaster = false;
 }
+const { post } = require("axios");
 
-let settings = {
-    token : '',
-    id :''
-};
+class abcApi {
+  constructor(token, id) {
+    if (!token) throw new Error("Unknown Token: Token Missing");
+    if (!id) throw new Error("Unknown ID: ID Missing");
+    if (typeof token !== "string")
+      throw new SyntaxError("Invalid ID: ID must be a String");
 
-module.exports = {
-    login: (token, id) => new Promise((resolve, reject) => {
+    if (typeof id !== "string")
+      throw new SyntaxError("Invalid ID: ID must be a String");
+    this.token = token;
+    this.id = id;
+  }
+  async postStats(client) {
+    if (!client) throw new Error("Unknown Client: Client Missing");
+    try {
+      const information = {
+        member_count: useDjsMaster
+          ? client.users.cache.size
+            ? client.users.cache.size
+            : 0
+          : client.users.size
+          ? client.users.size
+          : 0,
+        server_count: useDjsMaster
+          ? client.guilds.cache.size
+            ? client.guilds.cache.size
+            : 0
+          : client.guilds.size
+          ? client.guilds.size
+          : 0,
+        shard_count: client.shard
+          ? client.shard.count
+          : client.shards
+          ? client.shards.size
+          : 0
+      };
 
-        if (typeof token != 'string') { reject(new Error('[Arcane-wrapper] No token provided')); }
-        if(isNaN(id)) { reject(new Error('[Arcane-wrapper] No id provided'));  }
-
-        settings.token = token;
-        settings.id = id;
-
-        resolve('[Arcane-wrapper] Ready to post');
-    }),
-
-    post: (client) => new Promise(async (resolve, reject) => {
-        let users;
-        let guilds;
-
-        if(!client) { reject(new Error('[Arcane-wrapper] No library provided')); }
-        if (!settings.token) { reject(new Error('[Arcane-wrapper] No token provided, You must initialized the module')); }
-        if (!settings.id) { reject(new Error('[Arcane-wrapper] No id provided, You must initialized the module')); }
-        if(client.shard) {
-            users = await client.shard.fetchClientValues(useDjsMaster ? 'users.cache.size' : 'users.size'),
-                guilds = await client.shard.fetchClientValues(useDjsMaster ? 'guilds.cache.size' : 'users.size');
+      const content = JSON.stringify(information, null);
+      let data = await post(
+        `https://arcane-botcenter.xyz/api/${this.id}/stats`,
+        information,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: this.token
+          }
         }
+      );
 
-        let send = {
-            member_count : client.shard ? users.reduce((prev, val) => prev + val, 0) : (useDjsMaster ? (client.users.cache.size ? client.users.cache.size : 0) : (client.users.size ? client.users.size : 0)),
-            server_count : client.shard ? guilds.reduce((prev, val) => prev + val, 0) : (useDjsMaster ? (client.guilds.cache.size ? client.guilds.cache.size : 0) : (client.guilds.size ? client.guilds.size : 0)),
-            shard_count : client.shard ?  client.shard.count : client.shards ? client.shards.size : 0,
-        };
-
-        console.log('[Arcane-wrapper] Send Data in progress...');
-        sendData(send);
-    }),
-
-    update: (client) => {
-        setInterval(function () {
-            require('./index.js').post(client);
-        }, 600000)
+      return data;
+    } catch (err) {
+      throw new Error(err);
     }
-};
-
-function sendData(send) {
-    const content = JSON.stringify(send, null);
-    require('axios').post(`https://arcane-botcenter.xyz/api/${settings.id}/stats`, content, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': content.length,
-            'Authorization': settings.token
-        }
-    }).then((res) => {
-        if (res.status === 200){
-            console.log(`[Arcane-wrapper] Stats posted ! => https://arcane-botcenter.xyz/api/${settings.id}/stats \n\nGuild: ${send.server_count}\nUsers: ${send.member_count}\nShard: ${send.shard_count}`);
-        } else {
-            console.log('An error has occurred')
-        }
-    }).catch((err) => {
-        console.log(`[Arcane-wrapper] Stats post error ${err}`);
-    });
+  }
 }
+module.exports = abcApi
